eb66aa16434abb8062a8cad6ae1a90166df2a431
sjiang647 <sjiang647@gmail.com>
2021-05-04 19:21:52 -0700

    Add basic endpoints for song updates and geoqueries

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const { getAuthRoutes } = require("./auth.js");
+const { getSongRoutes } = require("./songs.js");
 
 function getRoutes() {
   const router = express.Router();
   router.use("/auth", getAuthRoutes());
+  router.use("/songs", getSongRoutes());
   return router;
 }
 
