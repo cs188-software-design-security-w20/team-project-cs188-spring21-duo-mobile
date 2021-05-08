daa375f85b5afcfae4593c7b7be4949535e4bd9f
sjiang647 <sjiang647@gmail.com>
2021-05-08 14:00:13 -0700

    Add eslint and refactor code to be lint compliant

--- backend/src/firebase-init.js
+++ backend/src/firebase-init.js
@@ -1,8 +1,8 @@
-const firebase = require("firebase/app");
-const admin = require("firebase-admin");
-require("@firebase/auth");
-require("@firebase/analytics");
-require("firebase/firestore");
+const firebase = require('firebase/app');
+const admin = require('firebase-admin');
+require('@firebase/auth');
+require('@firebase/analytics');
+require('firebase/firestore');
 require('dotenv').config();
 
 admin.initializeApp({
@@ -11,12 +11,12 @@ admin.initializeApp({
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "hopscotch-ddd21.firebaseapp.com",
-  projectId: "hopscotch-ddd21",
-  storageBucket: "hopscotch-ddd21.appspot.com",
-  messagingSenderId: "611116265466",
-  appId: "1:611116265466:web:15ad2e7e463293a8b67e43",
-  measurementId: "G-X0KLXFSKM8",
+  authDomain: 'hopscotch-ddd21.firebaseapp.com',
+  projectId: 'hopscotch-ddd21',
+  storageBucket: 'hopscotch-ddd21.appspot.com',
+  messagingSenderId: '611116265466',
+  appId: '1:611116265466:web:15ad2e7e463293a8b67e43',
+  measurementId: 'G-X0KLXFSKM8',
 };
 
 firebase.initializeApp(firebaseConfig);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,52 +1,52 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
-const { getRoutes } = require("./routes/index.js");
-
-function startServer({ port = process.env.PORT } = {}) {
-  const app = express();
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use("/api", getRoutes());
-  app.get("/test", (req, res) => {
-    console.log("test");
-    res.send("test");
-  });
-
-  return new Promise((resolve) => {
-    const server = app.listen(port, () => {
-      console.log(`Listening on port ${server.address().port}`);
-      const closeFunction = server.close.bind(server);
-      server.close = () => {
-        return new Promise((resolveClose) => {
-          closeFunction(resolveClose);
-        });
-      };
-      handleTerminate(server);
-      resolve(server);
-    });
-  });
-}
+const { getRoutes } = require('./routes/index.js');
 
 function handleTerminate(server) {
   async function exitHandler(options = {}) {
     await server
       .close()
       .then(() => {
-        console.log("Server successfully closed");
+        // eslint-disable-next-line no-console
+        console.log('Server successfully closed');
       })
       .catch((e) => {
-        console.log("Something went wrong closing the server");
+        // eslint-disable-next-line no-console
+        console.log('Something went wrong closing the server: ', e);
       });
     if (options.exit) process.exit();
   }
-  process.on("exit", exitHandler);
-  process.on("SIGINT", exitHandler.bind(null, { exit: true }));
-  process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
-  process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
-  process.on("uncaughtException", exitHandler.bind(null, { exit: true }));
+  process.on('exit', exitHandler);
+  process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+  process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+}
+
+function startServer({ port = process.env.PORT } = {}) {
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use('/api', getRoutes());
+  app.get('/test', (req, res) => {
+    res.send('test');
+  });
+
+  return new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Listening on port ${server.address().port}`);
+      const closeFunction = server.close.bind(server);
+      server.close = () => new Promise((resolveClose) => {
+        closeFunction(resolveClose);
+      });
+      handleTerminate(server);
+      resolve(server);
+    });
+  });
 }
 
 startServer();
