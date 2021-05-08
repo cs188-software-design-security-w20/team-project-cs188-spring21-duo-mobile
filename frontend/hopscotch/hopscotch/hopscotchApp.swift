732b8db4b91d58f8b3b0863749f363c434c2028a
Max Wu <chengmaxwu@gmail.com>
2021-05-08 03:51:43 -0700

    SwiftUI setup with Firebase authentication

--- frontend/hopscotch/hopscotch/hopscotchApp.swift
+++ frontend/hopscotch/hopscotch/hopscotchApp.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
+
+
+// https://peterfriese.dev/swiftui-new-app-lifecycle-firebase/
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
-struct hopscotchApp: App {
+struct HopscotchApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
