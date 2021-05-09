dfe8a2d784d089a572a23c96f58c0415386ce140
Sriram Balachandran <sriramb2000@gmail.com>
2021-05-08 18:41:32 -0700

    feat: attach spotify routes at api/spotify

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
-const { authMiddleware, getAuthRoutes } = require('./auth.js');
-const { getSongRoutes } = require('./songs.js');
+const { authMiddleware, getAuthRoutes } = require("./auth.js");
+const { getSongRoutes } = require("./songs.js");
+const { getSpotifyRoutes } = require("./spotify");
 
 function getRoutes() {
-  const router = express.Router();
-  router.use('/auth', getAuthRoutes());
-  router.use('/songs', authMiddleware, getSongRoutes());
-  return router;
+    const router = express.Router();
+    router.use("/auth", getAuthRoutes());
+    router.use("/songs", authMiddleware, getSongRoutes());
+    router.use("/spotify", getSpotifyRoutes());
+    return router;
 }
 
 module.exports = { getRoutes };
