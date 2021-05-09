557408f6e75b4d4b88ce1b4700edb61cd473781d
Sriram Balachandran <sriramb2000@gmail.com>
2021-05-08 18:56:59 -0700

    dev: fucking linting

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
-const { authMiddleware, getAuthRoutes } = require("./auth.js");
-const { getSongRoutes } = require("./songs.js");
-const { getSpotifyRoutes } = require("./spotify");
+const { authMiddleware, getAuthRoutes } = require('./auth.js');
+const { getSongRoutes } = require('./songs.js');
+const { getSpotifyRoutes } = require('./spotify');
 
 function getRoutes() {
-    const router = express.Router();
-    router.use("/auth", getAuthRoutes());
-    router.use("/songs", authMiddleware, getSongRoutes());
-    router.use("/spotify", getSpotifyRoutes());
-    return router;
+  const router = express.Router();
+  router.use('/auth', getAuthRoutes());
+  router.use('/songs', authMiddleware, getSongRoutes());
+  router.use('/spotify', getSpotifyRoutes());
+  return router;
 }
 
 module.exports = { getRoutes };
