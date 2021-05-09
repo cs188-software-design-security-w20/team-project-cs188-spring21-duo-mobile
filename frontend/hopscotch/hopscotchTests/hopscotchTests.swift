077b8de1a92e0902bd9c9e7d96956fa701d7d265
Max Wu <chengmaxwu@gmail.com>
2021-05-09 16:34:26 -0700

    Revert "Basic SwiftUI setup, including Firebase Authentication"

--- frontend/hopscotch/hopscotchTests/hopscotchTests.swift
+++ frontend/hopscotch/hopscotchTests/hopscotchTests.swift
@@ -2,7 +2,8 @@
 //  hopscotchTests.swift
 //  hopscotchTests
 //
-//  Created by Max Wu on 5/7/21.
+//  Created by Michael Wu on 5/4/21.
+//  Copyright © 2021 ucla-cs188. All rights reserved.
 //
 
 import XCTest
@@ -10,20 +11,20 @@ import XCTest
 
 class hopscotchTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
