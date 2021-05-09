3f197b2d274c41ec3367475c4929e761954c522a
Sriram Balachandran <sriramb2000@gmail.com>
2021-05-08 18:41:05 -0700

    feat: implement basic spotify-related routes

---
+++ backend/src/routes/spotify.js
@@ -0,0 +1,163 @@
+const express = require("express");
+const SpotifyWebApi = require('spotify-web-api-node');
+const crypto = require("crypto");
+const { firebase } = require("../firebase-init.js");
+const { authMiddleware } = require('./auth');
+const db = firebase.firestore();
+require("dotenv").config();
+
+const Spotify = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: `https://hopscotch-backend.herokuapp.com/api/spotify/oauth-callback`,
+});
+
+// Scopes to request.
+const SPOTIFY_SCOPES = [
+    'user-read-email',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-read-playback-state',
+    'user-read-currently-playing',
+    'user-library-read',
+    'user-top-read',
+];
+
+
+function getSpotifyRoutes() {
+    const router = express.Router();
+    router.get("/link", authMiddleware, handleLink);
+    // oauth-callback route doesn't have authMiddleware since it is called by Spotify
+    router.get("/oauth-callback", handleOauthCallback);
+    router.get("/currently-playing", authMiddleware, spotifyTokenMiddleware, handleCurrentlyPlaying);
+    return router;
+}
+
+/*
+-- Request Params --
+- redirectHost (OPTIONAL) -- If you are attempting to run this locally, you can pass in your local server host (e.g. "http://localhost:5000")
+    
+needs to be an authenticated request.
+
+-- Response --
+authorizeURL or error
+
+Creates a url that can be visited by the client to link Spotify account with 
+your Hopscotch account.
+*/
+async function handleLink(req, res) {
+    const state = crypto.randomBytes(20).toString('hex');
+    // Store the state in a document for lookup later, store the corresponding user's email
+    await db.collection("spotify_state").doc(state.toString()).set({ email: req.user_email, createdAt: "" });
+
+    if (req.query.redirectHost) {
+        Spotify.setRedirectURI(`${req.query.redirectHost}/api/spotify/oauth-callback`);
+    }
+    const authorizeURL = Spotify.createAuthorizeURL(SPOTIFY_SCOPES, state.toString());
+    res.json({ authorizeURL });
+}
+
+/*
+-- Request Params --
+- code -- This is the actual code that can be used with the Spotify SDK to get the user's refresh token
+- state -- This is the random string that we generated in the "link" route. It is used to figure out which user to 
+           associate these tokens with.
+
+-- Response --
+200 or error
+
+Stores the user's spotify refresh tokens in our database.
+*/
+async function handleOauthCallback(req, res) {
+    try {
+        const { code, state } = req.query;
+        if (!code || !state) {
+            throw new Error("Missing Spotify code or state token.");
+        }
+        //If no error, figure out whose spotify data this is using the state parameter
+        const spotifyStateRef = db.collection("spotify_state").doc(state);
+        const user_email = await spotifyStateRef.get().then((doc) => {
+            const { email } = doc.data();
+            return email;
+        });
+
+        Spotify.authorizationCodeGrant(code, (error, data) => {
+            if (error) {
+                throw error;
+            }
+            Spotify.setAccessToken(data.body['access_token']);
+
+            Spotify.getMe(async(error, userResults) => {
+                if (error) {
+                    throw error;
+                }
+                // We have a Spotify refresh token and the user identity now.
+                const refreshToken = data.body['refresh_token'];
+
+                // Get spotify data and store in the user's document
+                db.collection("user_metadata").doc(user_email).update({
+                    spotify_refresh_token: refreshToken
+                });
+
+                // Return success!
+                res.status(200).json({ success: true });
+            });
+        });
+    } catch (e) {
+        console.log("error", e);
+        res.status(500);
+    }
+}
+
+/*
+-- Request Property --
+  - user_spotify_refresh_token -- Requires the refresh_token so the Spotify API knows whose currently-playing data we are requesting.
+
+-- Response --
+  song or error
+  See https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-the-users-currently-playing-track for shape of song object.
+*/
+async function handleCurrentlyPlaying(req, res) {
+    try {
+        Spotify.setRefreshToken(req.user_spotify_refresh_token);
+        await Spotify.refreshAccessToken().then((data, err) => {
+            if (err) {
+                console.log('Could not refresh access token', err);
+            } else {
+                Spotify.setAccessToken(data.body['access_token']);
+            }
+        });
+        const song = await Spotify.getMyCurrentPlayingTrack().then((data, err) => {
+            return data.body.item;
+        });
+        res.status(200).json({ song });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: "Something went wrong while fetching the data" });
+    }
+}
+
+/*
+-- Request Property --
+  - user_email -- Requires the request.user_email property to be set so we know which user to fetch token for. This is set in the authMiddleware.
+
+  Attaches `user_spotify_refresh_token` property to the request that can be used downstream.
+*/
+async function spotifyTokenMiddleware(req, res, next) {
+    const { user_email } = req;
+    try {
+        req.user_spotify_refresh_token = await db.collection("user_metadata").doc(user_email).get().then((doc) => doc.data().spotify_refresh_token);
+        if (!req.user_spotify_refresh_token) {
+            throw new Error("No spotify token found.")
+        }
+        next();
+    } catch (e) {
+        res.status(400).json({ message: "You don't seem to have linked your account with spotify yet." })
+    }
+}
+
+
+
+module.exports = { getSpotifyRoutes };
