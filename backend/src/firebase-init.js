ff607c36c03d72a474fc15aac4ecaa28d6c69e82
Michael Wu <michaelywu54@gmail.com>
2021-05-09 17:50:53 -0700

    minor change firebase-init.js -- no PR

--- backend/src/firebase-init.js
+++ backend/src/firebase-init.js
@@ -6,7 +6,18 @@ require('firebase/firestore');
 require('dotenv').config();
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    "type": process.env.GOOGLE_CREDENTIALS_TYPE,
+    "project_id": process.env.GOOGLE_CREDENTIALS_PROJECT_ID,
+    "private_key_id": process.env.GOOGLE_CREDENTIALS_PRIVATE_KEY_ID,
+    "private_key": process.env.GOOGLE_CREDENTIALS_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.GOOGLE_CREDENTIALS_CLIENT_EMAIL,
+    "client_id": process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
+    "auth_uri": process.env.GOOGLE_CREDENTIALS_AUTH_URI,
+    "token_uri": process.env.GOOGLE_CREDENTIALS_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.GOOGLE_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.GOOGLE_CREDENTIALS_CLIENT_X509_CERT_URL
+  })
 });
 
 const firebaseConfig = {
