70ed11e6f199a729e93bb2a9af9b7eb16b414bfc
Max Wu <chengmaxwu@gmail.com>
2021-05-14 12:27:14 -0700

    Added refresh buttons

--- frontend/hopscotch-frontend/src/components/SongMap.js
+++ frontend/hopscotch-frontend/src/components/SongMap.js
@@ -2,13 +2,25 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import axios from "axios";
 import "firebase/firestore";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import ReactMapboxGl, { Layer, Feature, Marker, Popup } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import currentLocationIcon from "../assets/you_are_here.png";
 import songMarkerIcon from "../assets/marker.png";
 import { useAuth } from "../auth/authContext";
 import SongWidget from "./SongWidget";
+import {
+  Button,
+  Text,
+  Spacer,
+  Row,
+  Col,
+  Input,
+  Card,
+  Image,
+  Divider,
+  Modal,
+} from "@geist-ui/react";
 
 const Map = ReactMapboxGl({
   accessToken:
@@ -24,7 +36,7 @@ const SongMap = () => {
   const [viewport, setViewport] = useState({});
   const { getTokens } = useAuth();
 
-  useEffect(() => {
+  function refreshMap() {
     navigator.geolocation.getCurrentPosition(async ({ coords }) => {
       reCenter(coords);
       setUserLocation([coords.longitude, coords.latitude]);
@@ -41,6 +53,9 @@ const SongMap = () => {
           }
         });
     });
+  }
+  useEffect(() => {
+    refreshMap();
   }, []);
 
   const reCenter = (location, zoom) => {
@@ -61,50 +76,59 @@ const SongMap = () => {
       {userLocation == null ? (
         <div>Loading...</div>
       ) : (
-        <Map
-          {...viewport}
-          style="mapbox://styles/mapbox/streets-v9"
-          containerStyle={{
-            height: "600px",
-            width: "800px",
-          }}
-        >
-          <Marker coordinates={userLocation} anchor="bottom">
-            <img width="50" height="50" src={currentLocationIcon} />
-          </Marker>
-          {nearbySongs.map((song) => (
-            <Marker
-              coordinates={[song.lng, song.lat]}
-              anchor="bottom"
-              onClick={() => {
-                setSelectedSong(song);
-                setCenter([song.lng, song.lat]);
-              }}
-            >
-              <img width="25" height="25" src={songMarkerIcon} />
+        <Fragment>
+          <Button size="mini" onClick={refreshMap}>
+            Refresh Map
+          </Button>
+          <Map
+            {...viewport}
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+              height: "600px",
+              width: "800px",
+            }}
+          >
+            <Marker coordinates={userLocation} anchor="bottom">
+              <img width="50" height="50" src={currentLocationIcon} />
             </Marker>
-          ))}
-          {selectedSong && (
-            <Popup
-              key={selectedSong.locationHash}
-              coordinates={[selectedSong.lng, selectedSong.lat]}
-            >
-              <div>
-                <SongWidget songId={selectedSong.songData.id} height="80px" width="100%"/>
+            {nearbySongs.map((song) => (
+              <Marker
+                coordinates={[song.lng, song.lat]}
+                anchor="bottom"
+                onClick={() => {
+                  setSelectedSong(song);
+                  setCenter([song.lng, song.lat]);
+                }}
+              >
+                <img width="25" height="25" src={songMarkerIcon} />
+              </Marker>
+            ))}
+            {selectedSong && (
+              <Popup
+                key={selectedSong.locationHash}
+                coordinates={[selectedSong.lng, selectedSong.lat]}
+              >
                 <div>
-                  <button
-                    onClick={() => {
-                      setSelectedSong(null);
-                      setCenter(userLocation);
-                    }}
-                  >
-                    Close
-                  </button>
+                  <SongWidget
+                    songId={selectedSong.songData.id}
+                    height="80px"
+                    width="100%"
+                  />
+                  <div>
+                    <button
+                      onClick={() => {
+                        setSelectedSong(null);
+                        setCenter(userLocation);
+                      }}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </Popup>
-          )}
-        </Map>
+              </Popup>
+            )}
+          </Map>
+        </Fragment>
       )}
     </div>
   );
--- frontend/hopscotch-frontend/src/components/UserProfile.js
+++ frontend/hopscotch-frontend/src/components/UserProfile.js
@@ -4,7 +4,15 @@ import axios from "axios";
 import "firebase/firestore";
 import { useState, useEffect } from "react";
 import { useAuth } from "../auth/authContext";
-import { Button, Card, Collapse, Image, Spacer, Text, User } from "@geist-ui/react";
+import {
+  Button,
+  Card,
+  Collapse,
+  Image,
+  Spacer,
+  Text,
+  User,
+} from "@geist-ui/react";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
@@ -14,82 +22,114 @@ const UserProfile = () => {
   const [loading, setLoading] = useState(true);
   const { getTokens } = useAuth();
 
-  useEffect(() => {
-    async function fetchUserMetadata() {
-      try {
-        const tokens = await getTokens();
-        return axios.get(
-          `${BASE_URL}/api/users/me`,
-          { headers: tokens }
-        )
+  async function fetchUserMetadata() {
+    try {
+      const tokens = await getTokens();
+      return axios
+        .get(`${BASE_URL}/api/users/me`, { headers: tokens })
         .then((res) => {
-          console.log(res.data.userData)
-          console.log(res.data.userEntries)
+          console.log(res.data.userData);
+          console.log(res.data.userEntries);
           if (res.status === 200) {
             setUserData(res.data.userData);
             setUserEntries(res.data.userEntries);
             setLoading(false);
           }
         });
-      } catch (e) {
-        console.error(e);
-      }
+    } catch (e) {
+      console.error(e);
     }
+  }
+
+  useEffect(() => {
     fetchUserMetadata();
   }, []);
 
   return (
     <div>
-      {loading ? <div></div> :
-      <div>
+      {loading ? (
+        <div></div>
+      ) : (
         <div>
-          {userData.spotify_profile && userData.spotify_refresh_token ?
-            <Card width="100%">
-              <User src={userData.spotify_profile.images[0].url} name={userData.spotify_profile.display_name}>
-                {userData.phone}
-                <User.Link href={userData.spotify_profile.external_urls.spotify}>{userData.spotify_profile.uri}</User.Link>
-              </User>
-              <Spacer />
-              <Collapse title="Your song entries">
-                {userEntries.map(entry => (
-                  <div style={{display:"flex", flexWrap:"wrap"}}>
-                    <div style={{flex:"50%"}}><Image src={entry.songData.album.images[1].url} width={70} height={70} /></div>
-                    <div style={{flex:"50%"}}><Text small>
-                      <p>{entry.songData.name}<br/>
-                      {entry.songData.artists
-                      .map((artist) => artist.name)
-                      .join(", ")}</p>
-                    </Text></div>
-                  </div>
-                ))}
-              </Collapse>
-            </Card>
-            :
-            <Card width="100%">
-              <Button onClick={() => {
-                async function linkSpotify() {
-                  const tokens = await getTokens();
-                  return axios.get(
-                    `${BASE_URL}/api/spotify/link?redirectHost=${encodeURIComponent(
-                      BASE_URL
-                    )}`, { headers: tokens }
-                  )
-                  .then((res) => {
-                    if (res.status === 200) {
-                      window.open(res.data.authorizeURL, "_blank");
+          <div>
+            {userData.spotify_profile && userData.spotify_refresh_token ? (
+              <Card width="100%">
+                <User
+                  src={userData.spotify_profile.images[0].url}
+                  name={userData.spotify_profile.display_name}
+                >
+                  {userData.phone}
+                  <User.Link
+                    href={userData.spotify_profile.external_urls.spotify}
+                  >
+                    {userData.spotify_profile.uri}
+                  </User.Link>
+                </User>
+                <Button
+                  size="mini"
+                  onClick={() => {
+                    fetchUserMetadata();
+                  }}
+                >
+                  Refresh
+                </Button>
+                <Spacer />
+                <Collapse title="Your song entries">
+                  {userEntries.map((entry) => (
+                    <div style={{ display: "flex", flexWrap: "wrap" }}>
+                      <div style={{ flex: "50%" }}>
+                        <Image
+                          src={entry.songData.album.images[1].url}
+                          width={70}
+                          height={70}
+                        />
+                      </div>
+                      <div style={{ flex: "50%" }}>
+                        <Text small>
+                          <p>
+                            {entry.songData.name}
+                            <br />
+                            {entry.songData.artists
+                              .map((artist) => artist.name)
+                              .join(", ")}
+                          </p>
+                        </Text>
+                      </div>
+                    </div>
+                  ))}
+                </Collapse>
+              </Card>
+            ) : (
+              <Card width="100%">
+                <Button
+                  onClick={() => {
+                    async function linkSpotify() {
+                      const tokens = await getTokens();
+                      return axios
+                        .get(
+                          `${BASE_URL}/api/spotify/link?redirectHost=${encodeURIComponent(
+                            BASE_URL
+                          )}`,
+                          { headers: tokens }
+                        )
+                        .then((res) => {
+                          if (res.status === 200) {
+                            window.open(res.data.authorizeURL, "_blank");
+                          }
+                        });
                     }
-                  });
-                }
-                linkSpotify();
-              }}
-              >
-                Link Spotify
-              </Button>
-            </Card>}
+                    linkSpotify();
+                  }}
+                >
+                  Link Spotify
+                </Button>
+              </Card>
+            )}
+          </div>
         </div>
-      </div>}
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default UserProfile;
